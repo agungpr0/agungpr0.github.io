e7b38dda81b40d7808a00350903cf616a70ab930
agungpr0 <75931418+agungpr0@users.noreply.github.com>
2021-03-25 03:23:18 +0700

    Create script.js

---
+++ portfolio/js/script.js
@@ -0,0 +1,25 @@
+const job = ['Web Designer', 'Digital Creator','Freelancer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == job.length){
+		count = 0;
+	}
+
+	currentTxt = job[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+	document.querySelector('.efek-ngetik').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 300);
+
+})();
